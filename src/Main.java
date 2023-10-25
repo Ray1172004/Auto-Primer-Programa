33ad03cde232e1562bd6ca07de2cb71c789c2b2b
Ray <mathius2417@gmail.com>
2023-10-25 07:46:25 -0500

    Conexiones para almacenar datos

--- src/Main.java
+++ src/Main.java
@@ -1,39 +1,35 @@
-import java.util.Scanner;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class Main {
     public static void main(String[] args) {
-        Auto auto1 = new Auto();
-        auto1.setPuertas(10);
-        auto1.setColor("Azul");
-        auto1.imprimirColorPuertas();
-
-        //Auto auto2 = new Auto( 4,  "Rojo ",  3);
-        //auto2.imprimirColorPuertas();
-
-        Dueno dueno1 = new Dueno("Negro", 1,19, 172422674, "Ecuatoriano", 72.5, "O+");
-        //dueno1.imprimirDueno();
-
-        Motor motor2 = new Motor(6.4, "Disel");
-        motor2.imprimirMotor();
-       System.out.println("--------------------------------------------------------------------------------");
-        auto1.setMotor(motor2);
-        auto1.getMotor().imprimirMotor();
-        auto1.setDueno(dueno1);
-        auto1.getDueno().imprimirDueno();
-
-
-
-
-
-        //System.out.println("");
-
-        //Dueno dueno2 = new Dueno("Blanco", 1, 17, 28392183012, "Americano");
-        //Motor motor2 = new Motor();
-
-
-
-
-
+        Dueno dueno1 = new Dueno("negro",2,19,17242657,"Ecuador",5.6,"0+");
         
+        Motor motor1 = new Motor(2,"atun");
+        Motor motor2 = new Motor(2,"arroz");
+        
+        Auto auto1 = new Auto();
+        Auto auto2 = new Auto();
+        
+        Map<String, Auto> mapAutos = new HashMap<>();
+
+        mapAutos.put("Negro",auto1);
+        mapAutos.put("rjo",auto1);
+        mapAutos.put("azul",auto1);
+
+        System.out.println("El auto es color negro tiene: "+mapAutos.get("Negro").getNumeroRuedas()+"de ruedas");
+        System.out.println("El auto es color rjo tiene: "+mapAutos.get("rjo").getNumeroRuedas()+"de ruedas");
+
+        mapAutos.remove("rjo");
+//Conectores (buscar e invetigar para entender de una mejor manera)
+        List<Auto> listaAutos =
+
+
+
+
+
+
 
     }
 }
